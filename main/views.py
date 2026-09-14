697cd4d21d86f2efaa6e78c18f304d66c37928c9
unknown <azzam.zawawi@ui.ac.id>
2026-09-14 23:03:07 +0700

    fix: update full name in context views

--- main/views.py
+++ main/views.py
@@ -17,7 +17,7 @@ def show_main(request):
 
 def show_experience(request):
     context = {
-        "name": "Azzam",
+        "name": "Azzam Zawawi Al Rasyid",
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
